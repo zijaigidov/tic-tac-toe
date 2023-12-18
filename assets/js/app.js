bba17491cabf1a3765b054ddec138a81d55b3419
zijaigidov <igidov.zija@gmail.com>
2023-12-18 20:43:59 +0100

    Add module for game board
    Added a module for the game board containing methods to manipulate the
    Gameboard object, such as marking a square, printing and resetting the
    board.

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+// This module contains the logic for the game board
+const Gameboard = (function () {
+  const board = [];
+  const BOARD_SIZE = 3;
+
+  // Create the 2D array for the board
+  for (let row = 0; row < BOARD_SIZE; row++) {
+    board[row] = [];
+    for (let col = 0; col < BOARD_SIZE; col++) {
+      board[row].push(Square());
+    }
+  }
+
+  const markSquare = (row, column, mark) => {
+    const square = board[row][column];
+    if (!square.getValue()) square.setValue(mark);
+  };
+
+  /* This method will only be used in the console version and will be
+     removed later on */
+  const printBoard = () => {
+    const boardWithValues = board.map((row) =>
+      row.map((square) => square.getValue()),
+    );
+    console.log(boardWithValues);
+  };
+
+  const resetBoard = () => {
+    for (let row = 0; row < BOARD_SIZE; row++) {
+      for (let col = 0; col < BOARD_SIZE; col++) {
+        board[row][col].setValue('');
+      }
+    }
+  };
+
+  function Square() {
+    let value = '';
+
+    const setValue = (newValue) => {
+      value = newValue;
+    };
+    const getValue = () => value;
+    return { setValue, getValue };
+  }
+
+  return { markSquare, printBoard, resetBoard };
+})();
